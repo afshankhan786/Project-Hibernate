c7009f150d1eb48ea63f2922b343ca8667eefa92
afshankhan786 <profit.afshankhan@gmail.com>
2026-01-23 21:14:13 +0530

    update

--- src/main/java/com/project/main/EmployeeRunner.java
+++ src/main/java/com/project/main/EmployeeRunner.java
@@ -8,6 +8,7 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import com.project.config.EmpConfiguration;
 import com.project.entity.Employee;
@@ -16,16 +17,29 @@ public class EmployeeRunner {
 
 	public static void main(String[] args){
 		
-		Employee emp = new Employee("ranus", "male", 5055);
+		Employee emp = new Employee("ranu", "female", 5055);
+		Employee emp2 = new Employee("mannu", "male", 5055);
+		Employee emp3 = new Employee("vikas", "male", 5055);
+		Employee emp4 = new Employee("vivek", "male", 5055);
+		Employee emp5 = new Employee("suman", "female", 5055);
+
 		 
 		
 		Session session = EmpConfiguration.getSessionFactory().openSession();
 		Transaction tx = session.beginTransaction();
+//		
+//		session.persist(emp);
+//		session.persist(emp2);
+//		session.persist(emp3);
+//		session.persist(emp4);
+//		session.persist(emp5);
+//		tx.commit();
 		
+		Query<Employee> query = session.createQuery("from empp",Employee.class);
 		
+		System.out.println(query.list());
 		
-		session.persist(emp);
-		tx.commit();
+
 		
 //		Employee employee = session.find(Employee.class,9);
 //		System.out.println(employee);
